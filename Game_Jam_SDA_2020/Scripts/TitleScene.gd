b570b3dd783ee514d470cbd1287e01b3c688f17a
clemetayer <clemetayer@enseirb-matmeca.fr>
2020-04-24 23:39:46 +0200

    added title scene and animations

---
+++ Game_Jam_SDA_2020/Scripts/TitleScene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const scene1 = preload("res://Scenes/Scene1.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass 
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene_to(scene1)
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
